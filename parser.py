a1fb3f2fd21c7c207334c12c329debb4902291af
renroyz <81305041+renroyz@users.noreply.github.com>
2022-08-28 12:00:21 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,27 +7,29 @@ Original file is located at
     https://colab.research.google.com/drive/1J1ciVypGZknecx7CMFs1jhAMJRU8ZT6l
 """
 
+#installing pypdf2
 pip install pypdf2
 
+#importing pypdf2
 import PyPDF2
 
+#opening pdf file
 pdfFileObj = open('/resume.pdf', 'rb')
 
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-pageobj = pdfReader.getPage(0)
+print(pdfReader.numPages)
 
-print(pageobj.extractText())
+text=''
+for i in range(0,pdfReader.numPages):
+    # creating a page object
+    pageObj = pdfReader.getPage(i)
+    # extracting text from page
+    text=text+pageObj.extractText()
+print(text)
 
-text=pageobj.extractText()
-
-import re
-
-EMAIL_REG = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
-
-re.findall(EMAIL_REG, text)
-
+#finding phone number
 ph = re.compile(r"\d\d\d\d\d\d\d\d\d\d\d\d")
 
 a=re.findall(ph,text)
@@ -36,18 +38,41 @@ if len(a)==0:
   a=re.findall(ph,text)
 print(a)
 
-import csv
+#installing spacy
+!pip install spacy
 
-import nltk
-nltk.download('punkt')
+!python -m spacy download en_core_web_lg
 
-from nltk import word_tokenize
+import spacy
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_lg")
 
-tk=word_tokenize(text)
+# Process whole documents
+doc = nlp(text)
 
-l=[]
-with open('/skills.csv', 'r') as f:
-  reader = csv.reader(f)
-  for j in text:
-    if j in reader:
-      l.append(j)
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
+
+#import spacy
+import spacy
+nlp = spacy.load("en_core_web_lg")
+tokens = nlp(text)
+#finding name
+PERSON=[t for t in tokens.ents if t.label_ == 'PERSON']
+print('Name: ',PERSON[0])
+#finding phone number
+phone=[t for t in tokens.ents if t.label_ == 'CARDINAL']
+print('Phone number: ',phone[0])
+#finding email
+import re
+E = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
+email=re.findall(E, text)
+print('Email: ',email)
+#finding skills
+skills=[t for t in tokens.ents if t.label_ == 'ORG']
+print('Skills: ',skills)
+# import csv
+# with open('/skills.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     sk=re.findall(reader,skills)
